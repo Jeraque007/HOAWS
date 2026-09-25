38e619877bd4935381d61f2df3b3e7baa5e93af3
Jeraque007 <jeraque007@hotmail.com>
2026-09-25 09:34:28 +0200

    Deploy both Hoaws hero logo assets

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,9 @@ import { ArrowUpRight, Check, ChevronRight, Download, ExternalLink, Instagram, M
 import { saveReview, getApprovedReviews } from './supabase'
 import ReviewAdmin from './ReviewAdmin'
 import TesseraLumen from './TesseraLumen'
-const homeLogo = '/Hoaws_Translucent.png?v=2'
-const heroLogo = '/Hoaws%20Name%20Logo.png?v=2'
+const floatingLogo = '/Hoaws_Translucent.png?v=3'
+const homeLogo = floatingLogo
+const heroLogo = '/Hoaws%20Name%20Logo.png?v=3'
 
 const digitalCardFront = '/Hoaws.VCF.png'
 const whatsapp = 'https://wa.me/27740145161?text=Hi%20HOAWS%2C%20I%27d%20like%20to%20start%20a%20project.'
@@ -258,6 +259,9 @@ function Home() {
             <div className="hero-orbit orbit-two" />
             <div className="hero-brand-lockup">
               <img src={heroLogo} className="hero-name-logo" alt="Hoaws Name Logo" />
+            </div>
+            <div className="floating-logo-mark" aria-hidden="true">
+              <img src={floatingLogo} alt="" />
             </div>
             <span className="visual-caption">
               01 / The digital partner<br />
